3a52d67f4dae7d872f8051207524f7bc891bff89
hvupham <21522804@gm.uit.edu.vn>
2024-11-03 14:41:12 +0700

    push lab2 bai2

---
+++ LAB2/21522804_Bai2/src/components/Option.tsx
@@ -0,0 +1,51 @@
+// Option.tsx
+import React from "react";
+import { View, Text, Switch, StyleSheet } from "react-native";
+import { themState } from "../../App";
+
+type OptionProps = {
+  theme: themState;
+  onSetTheme: () => void;
+  isNotifications: boolean;
+  onSetIsNotifications: () => void;
+};
+
+const Option: React.FC<OptionProps> = ({ theme, onSetTheme, isNotifications, onSetIsNotifications }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.optionRow}>
+        <Text style={[styles.optionText, { color: theme.TEXT_COLOR }]}>Dark Mode</Text>
+        <Switch
+          value={theme.STATE === "light" ? false : true}
+          onValueChange={onSetTheme}
+        />
+      </View>
+      <View style={styles.optionRow}>
+        <Text style={[styles.optionText, { color: theme.TEXT_COLOR }]}>Notifications</Text>
+        <Switch
+          value={isNotifications}
+          onValueChange={onSetIsNotifications}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "column",
+    padding: 8,
+    marginTop: 8,
+  },
+  optionRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginVertical: 4,
+  },
+  optionText: {
+    fontSize: 18, // Thay thế cho text-lg
+  },
+});
+
+export default Option;
