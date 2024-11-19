ba7d397fcc37482ee834e7b23b24120d3bd789a5
hvupham <21522804@gm.uit.edu.vn>
2024-11-19 21:48:07 +0700

    a

--- LAB3/21522804_Bai2/src/Components/ListItem.tsx
+++ LAB3/21522804_Bai2/src/Components/ListItem.tsx
@@ -1,67 +1,8 @@
 import { Text, View, ScrollView, TouchableOpacity, StyleSheet } from "react-native"
-import { EvilIcons } from "@expo/vector-icons"
 import { useRecoilValue, useSetRecoilState } from "recoil"
 import { listNoteState, themeState, updateNoteState } from "../stores/atom"
 import React from "react"
-import { NavigationProp, useNavigation } from "@react-navigation/native"
-import { HomeNativeStackParamList } from "../Navigation/Type"
-import hooksNote from "../hooks/hookNote/hooksNote"
-
-type TItemNoteScreenComponentProps = {
-  id: string
-  title: string
-  note: string
-}
-
-export const ItemNoteScreenComponent: React.FC<TItemNoteScreenComponentProps> = (props) => {
-  const { title, note, id } = props
-  const navigation = useNavigation<NavigationProp<HomeNativeStackParamList, "NoteApp">>()
-  const { theme, fontSize } = useRecoilValue(themeState)
-  const setUpdateNote = useSetRecoilState(updateNoteState)
-  const { handleDeleteNote } = hooksNote.useDeleteNote()
-  const { handleGetListNote } = hooksNote.useGetListNote()
-
-  const handleSetUpdateNote = () => {
-    setUpdateNote({
-      state: "hasValue",
-      content: { id, title, note },
-    })
-  }
-
-  return (
-    <TouchableOpacity
-      style={styles.touchable}
-      onPress={() => {
-        handleSetUpdateNote()
-        navigation.navigate("NoteApp", { id })
-      }}
-    >
-      <View
-        style={[styles.noteContainer, { borderColor: theme === "Dark" ? "#ffffff" : "#A0A0A0" }]}
-      >
-        <View style={styles.textContainer}>
-          <Text style={[styles.titleText, { fontSize, color: theme === "Dark" ? "white" : "black" }]}>
-            {title}
-          </Text>
-          <Text style={[styles.noteText, { fontSize, color: theme === "Dark" ? "white" : "black" }]}>
-            {note}
-          </Text>
-        </View>
-        <TouchableOpacity
-          onPress={() => {
-            handleDeleteNote(id)
-            handleGetListNote()
-          }}
-        >
-          <View>
-            <EvilIcons name="trash" size={42} color={theme === "Dark" ? "white" : "black"} />
-          </View>
-        </TouchableOpacity>
-      </View>
-    </TouchableOpacity>
-  )
-}
-
+import { ItemNoteScreenComponent } from "./Item"
 export const ListItemNoteScreenComponent = () => {
   const { state, contents } = useRecoilValue(listNoteState)
   return (
